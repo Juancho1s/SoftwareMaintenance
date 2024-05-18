74c18aa5779c17290d39480ab5b04e2fa89cee8a
Juan Valenzuela <113451623+Juancho1s@users.noreply.github.com>
2024-05-18 09:56:08 -0600

    Controllers and models

---
+++ general.js
@@ -0,0 +1,12 @@
+class General {
+  dbCredentials = {
+    host:       "localhost",
+    username:   "root",
+    password:   "123456789",
+    database:   "elevator_management_system",
+    dialect:    "mysql",
+    define: {
+      timestamps: false,
+    },
+  };
+}
