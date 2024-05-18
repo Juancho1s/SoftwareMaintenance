9dc53951020c0d18342728026672d730bcbe71d2
Juan Valenzuela <113451623+Juancho1s@users.noreply.github.com>
2024-05-18 11:38:08 -0600

    Models correction and foreign keys addition

--- general.js
+++ general.js
@@ -1,12 +1,12 @@
-class General {
-  dbCredentials = {
-    host:       "localhost",
-    username:   "root",
-    password:   "123456789",
-    database:   "elevator_management_system",
-    dialect:    "mysql",
-    define: {
-      timestamps: false,
-    },
-  };
-}
+const dbCredentials = {
+  host: "localhost",
+  username: "root",
+  password: "123456789",
+  database: "elevator_management_system",
+  dialect: "mysql",
+  define: {
+    timestamps: false,
+  },
+};
+
+module.exports = { dbCredentials };
---
+++ models/ElevatorORM.js
@@ -0,0 +1,72 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { dbCredentials } = require("../general");
+
+const sequelize = new Sequelize(dbCredentials);
+
+const elevatorORM = sequelize.define("elevator", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true,
+  },
+  current_floor: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  alarm: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  door_state: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+  },
+});
+
+const direction_listORM = sequelize.define("direction_list", {
+  direction_floor_number: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  elevator_direction_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: elevatorORM, // This references the table name directly
+      key: "id",
+    },
+  },
+});
+
+const carry_listORM = sequelize.define("carry_list", {
+  stop_floor_number: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  elevatro_stop_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: elevatorORM, // This references the table name directly
+      key: "id",
+    },
+  },
+});
+
+// Set up associations
+elevatorORM.hasMany(direction_listORM, {
+    foreignKey: 'elevator_direction_id',
+    as: 'directions'
+  });
+  
+  direction_listORM.belongsTo(elevatorORM, {
+    foreignKey: 'elevator_direction_id',
+    as: 'elevator'
+  });
+
+module.exports = {elevatorORM};
